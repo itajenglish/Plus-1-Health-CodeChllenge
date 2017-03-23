664ef53b4734b7d4d783d49183c7e0ea1e2ec605
Taj English <tajenglish@gmail.com>
2017-03-23 18:52:45 -0400

    Added New Gallery Form

--- controllers/galleries.js
+++ controllers/galleries.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//Renders Page To Add NEW Gallery
+router.get('/new', (req, res, next) => {
+  res.render('gallery/new');
+})
+
+
 
 module.exports = router;
